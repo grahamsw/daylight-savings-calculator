db5ac55f56c4af6a5fcc55ef289019d0d6939ee8
grahamsw <grahams@pipeline.com>
2026-03-06 12:51:22 -0500

    style: remove glitch animation from header

--- src/App.vue
+++ src/App.vue
@@ -56,49 +56,6 @@ header {
   text-shadow: 0 0 10px rgba(0, 255, 204, 0.8), 0 0 20px rgba(0, 255, 204, 0.5);
 }
 
-.glitch::before,
-.glitch::after {
-  display: block;
-  content: attr(data-text);
-  position: absolute;
-  top: 0;
-  left: 0;
-  opacity: 0.8;
-}
-
-.glitch::before {
-  animation: glitch-color 0.3s cubic-bezier(0.25, 0.46, 0.45, 0.94) both infinite;
-  color: #0ff;
-  z-index: -1;
-}
-
-.glitch::after {
-  animation: glitch-color 0.3s cubic-bezier(0.25, 0.46, 0.45, 0.94) reverse both infinite;
-  color: #ff00ff;
-  z-index: -2;
-}
-
-@keyframes glitch-color {
-  0% {
-    transform: translate(0);
-  }
-  20% {
-    transform: translate(-2px, 2px);
-  }
-  40% {
-    transform: translate(-2px, -2px);
-  }
-  60% {
-    transform: translate(2px, 2px);
-  }
-  80% {
-    transform: translate(2px, -2px);
-  }
-  100% {
-    transform: translate(0);
-  }
-}
-
 main {
   width: 100%;
 }
